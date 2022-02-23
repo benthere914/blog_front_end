ea2aed9e4a11d38ba861085df843a096cf0ab10c
Benjamin Rose <benthere914@gmail.com>
2022-02-23 16:05:47 -0600

    created basic forms for sign up and log in

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,9 +1,22 @@
 import './index.css'
-
+import { useState } from 'react'
+import { Alert, Button } from 'react-bootstrap'
 const LogIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <>
-            <p>at the login page</p>
+            <div className='authForm'>
+                <Alert>
+                    <Alert.Heading>Log In</Alert.Heading>
+                        <p>Email</p>
+                        <input autoComplete='off' value={email} onChange={(e) => {setEmail(e.target.value)}}></input>
+                        <p>password</p>
+                        <input autoComplete='off' type='password' value={password} onChange={(e) => {setPassword(e.target.value)}}></input>
+                        <br/>
+                        <Button>Log In</Button>
+                </Alert>
+            </div>
         </>
     )
 }
--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -1,9 +1,30 @@
+import { useState } from 'react'
+import { Alert, Button } from 'react-bootstrap'
 import './index.css'
 
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
     return (
         <>
-            <p>at the sign up page</p>
+            <div className='authForm'>
+                <Alert>
+                    <Alert.Heading>Sign Up</Alert.Heading>
+                        <p>Email</p>
+                        <input autoComplete='off' value={email} onChange={(e) => {setEmail(e.target.value)}}></input>
+                        <p>username</p>
+                        <input autoComplete='off' value={username} onChange={(e) => {setUsername(e.target.value)}}></input>
+                        <p>password</p>
+                        <input autoComplete='off' type='password' value={password} onChange={(e) => {setPassword(e.target.value)}}></input>
+                        <p>confirm password</p>
+                        <input type='password' value={confirmPassword} onChange={(e) => {setConfirmPassword(e.target.value)}}></input>
+                        <br/>
+                        <Button>Sign Up</Button>
+                </Alert>
+            </div>
         </>
     )
 }
